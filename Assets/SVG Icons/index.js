ddc8c891d53b5f0909cfde720378c61cbd04d01d
Shashidhar7407 <shashank.papishetty@gmail.com>
2021-11-16 23:26:06 +0530

    yescommit

---
+++ Assets/SVG Icons/index.js
@@ -0,0 +1,17 @@
+let headingContentELement = document.getElementById("heading-Content");
+let descriptionContentElement = document.getElementById("description-Content");
+
+let headingContentELement2 = "Dolor sir Amet";
+let descriptionContentElement2 =
+  "velit esse cilium dolore eu fugiat nulla pariatur. Excepteur sint accaecat cupidatat non proident. sunt in culpa qui officia.";
+
+let headingContentELement3 = "Velit Esse Cillum";
+let descriptionContentElement3 =
+  "esse cillum dolore eu fugiat n. Exceptor sint occaecat cupiadatat non proident sunt in culpa qui officia.";
+
+function changeCoarousel() {
+  document.getElementById("heading-Content").textContent =
+    headingContentELement2;
+  document.getElementById("description-Content").textContent =
+    descriptionContentElement2;
+}
